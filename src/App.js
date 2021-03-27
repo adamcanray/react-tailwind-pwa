0deb6715a392f5db0c1a954d139dc3110b807ca5
adamcanray <adamcool180@gmail.com>
2021-03-27 19:09:08 +0700

    style: showing a card for testing tailwind classnames and theme.extends.colors

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="min-h-screen bg-gray-200 flex justify-center items-center">
+      <div class="max-w-2xl bg-regal-blue border-2 border-gray-300 p-5 rounded-md tracking-wide shadow-lg">
+          <div id="header" class="flex"> 
+            <img alt="mountain" class="w-45 rounded-md border-2 border-gray-300" src="https://picsum.photos/seed/picsum/200" />
+            <div id="body" class="flex flex-col ml-5">
+                <h4 id="name" class="text-xl font-semibold mb-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit</h4>
+                <p id="job" class="text-gray-800 mt-2">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <div class="flex mt-5">
+                  <img alt="avatar" class="w-6 rounded-full border-2 border-gray-300" src="https://picsum.photos/seed/picsum/200" />
+                  <p class="ml-3">John Doe</p>
+                </div>
+            </div>
+          </div>
+      </div>
     </div>
   );
 }
