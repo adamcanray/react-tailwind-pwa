452cdfff76d4cc53e79303501b9fdada0b278aac
adamcanray <adamcool180@gmail.com>
2021-04-25 21:43:31 +0700

    feat(app): add some Route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import React from 'react'
-import Masonry from './pages/masonry'
+import React, { lazy, useEffect } from 'react'
+import {
+  Switch,
+  Route,
+  Redirect,
+  // useLocation
+} from 'react-router-dom'
+
+
+const Layout = lazy(() => import('./containers/Layout'))
+const Login = lazy(() => import('./pages/Login'))
+const Masonry = lazy(() => import('./pages/Masonry'))
 
 function App() {
+  // const location = useLocation()
+  
+  // useEffect(() => {
+  //   if(location.pathname.toLowerCase().includes('app')){
+  //     console.log(location)
+  //     console.log('di app')
+  //     console.log("User-agent header sent: " + navigator.userAgent)
+  //   }
+  //   else if(location.pathname.toLowerCase().includes('login')){
+  //     console.log('masuk login')
+  //     console.log("User-agent header sent: " + navigator.userAgent)
+  //   }
+  // })
+
   return (
-    <div className="container mx-auto min-h-screen">
-      <Masonry/>
-    </div>
+    <>
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/masonry" component={Masonry} />
+        <Route path="/app" component={Layout} />
+        <Redirect exact from="/" to="/app" />
+      </Switch>
+    </>
   );
 }
 
