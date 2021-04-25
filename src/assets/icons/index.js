4d99c7dda730aae31a40a267feb4a6a7d4af2019
adamcanray <adamcool180@gmail.com>
2021-04-25 21:36:00 +0700

    feat(icons): setup a ready to use icons

---
+++ src/assets/icons/index.js
@@ -0,0 +1,19 @@
+import { ReactComponent as SunIcon } from './sun.svg'
+import { ReactComponent as MoonIcon } from './moon.svg'
+import { ReactComponent as PresentationChartBarIcon } from './presentation-chart-bar.svg'
+import { ReactComponent as CollectionIcon } from './collection.svg'
+import { ReactComponent as LoginIcon } from './login.svg'
+import { ReactComponent as MenuIcon } from './menu.svg'
+import { ReactComponent as AdjusmentsIcon } from './adjusments.svg'
+import { ReactComponent as ViewGridIcon } from './view-grid.svg'
+
+export {
+  SunIcon,
+  MoonIcon,
+  PresentationChartBarIcon,
+  CollectionIcon,
+  LoginIcon,
+  MenuIcon,
+  AdjusmentsIcon,
+  ViewGridIcon,
+}
