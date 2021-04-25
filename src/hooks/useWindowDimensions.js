abd29b67d19eefea2dd8ac9238c234500621eb5e
adamcanray <adamcool180@gmail.com>
2021-04-25 21:29:59 +0700

    feat(hooks): setup custom hooks

---
+++ src/hooks/useWindowDimensions.js
@@ -0,0 +1,28 @@
+/**
+ * Get current window dimensions (width/height), reference source: https://stackoverflow.com/a/36862446/11587161
+ * 
+ */
+import { useState, useEffect } from 'react';
+
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height
+  };
+}
+
+export default function useWindowDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return windowDimensions;
+}
