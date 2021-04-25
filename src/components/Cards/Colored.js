819d661347723a219ae0a36dfcb20733b04baa8d
adamcanray <adamcool180@gmail.com>
2021-04-25 21:41:05 +0700

    feat(colored card): add colored card component

--- src/components/Cards/Colored.js
+++ src/components/Cards/Colored.js
@@ -17,7 +17,6 @@ export default function CardColored({
    'banana',
    'cherry',
    'cucumber',
-   'mangosteen',
  ]
  /**
   * Override the `type` props with random element from `colorTypes`
@@ -29,25 +28,24 @@ export default function CardColored({
     <div className={classNames(
       'p-6 sm:rounded-lg shadow-sm border w-full',
       {
-        'bg-white': !type||type==='jicama',
+        'bg-white dark:bg-gray-800': !type||type==='jicama',
         'bg-orange-500': type==='carrot',
         'bg-yellow-500': type==='banana',
         'bg-red-500': type==='cherry',
         'bg-green-500': type==='cucumber',
-        'bg-gray-800': type==='mangosteen',
       }
     )}>
       <h4 className={classNames(
         'text-3xl font-semibold',
         {
-          'text-gray-700': !type||type==='jicama',
+          'text-gray-700 dark:text-gray-400': !type||type==='jicama',
           'text-white': type==='carrot'||type==='banana'||type==='cherry'||type==='cucumber'||type==='mangosteen',
         }
       )}>{title}</h4>
       <p className={classNames(
         'text-lg mt-2',
         {
-          'text-gray-500': !type||type==='jicama',
+          'text-gray-500 dark:text-gray-300': !type||type==='jicama',
           'text-white': type==='carrot'||type==='banana'||type==='cherry'||type==='cucumber'||type==='mangosteen',
         }
       )}>{text}</p>
