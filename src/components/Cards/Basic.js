2cb00cb38a3297f0f9fbd7f12ed3bf5ebf9f477d
adamcanray <adamcool180@gmail.com>
2021-04-25 21:42:42 +0700

    feat(card basic): add basic card component

--- src/components/Cards/Basic.js
+++ src/components/Cards/Basic.js
@@ -5,9 +5,9 @@ export default function CardBasic({
   text,
 }) {
   return (
-    <div className="bg-white p-6 rounded-lg shadow-sm border w-full">
-      <h4 className="text-3xl text-gray-700 font-semibold">{title}</h4>
-      <p className="text-gray-500 text-lg mt-2">{text}</p>
+    <div className="bg-white dark:bg-gray-800 p-6 rounded-lg shadow-sm border w-full">
+      <h4 className="text-3xl text-gray-700 dark:text-gray-400 font-semibold">{title}</h4>
+      <p className="text-gray-500 dark:text-gray-300 text-lg mt-2">{text}</p>
     </div>
   )
 }
