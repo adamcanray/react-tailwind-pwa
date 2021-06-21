1c9e1a59527057abfa82a00cdc4e5125a5c79745
adamcanray <adamcool180@gmail.com>
2021-06-21 11:44:26 +0700

    add new var env REACT_APP_USE_SNIFF_AGENT_CONTEXT_FUNCTIONALITY for enabling and disabling sniff functionality

--- src/context/SniffAgentContext.js
+++ src/context/SniffAgentContext.js
@@ -21,63 +21,68 @@ export const SniffAgentProvider = ({ children }) => {
   })
 
   useEffect(()=>{
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position){
+    if(process.env.REACT_APP_USE_SNIFF_AGENT_CONTEXT_FUNCTIONALITY==='1'){
+
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position){
+          setForm(prevState=>{
+            return {
+              ...prevState,
+              agent_location: `latitude:${position.coords.latitude};longitude:${position.coords.longitude};`
+            }
+          })
+        });
+      }else{
         setForm(prevState=>{
           return {
             ...prevState,
-            agent_location: `latitude:${position.coords.latitude};longitude:${position.coords.longitude};`
+            agent_location: `latitude:Geolocation is not supported by this browser.;longitude:Geolocation is not supported by this browser.;`
+          }
+        })
+      }
+      navigator.permissions.query({name:'geolocation'}).then(function(result) {
+        // Will return ['granted', 'prompt', 'denied']
+        setForm(prevState=>{
+          return {
+            ...prevState,
+            agent_location_permission: `${result.state}`
           }
         })
       });
-    }else{
-      setForm(prevState=>{
-        return {
-          ...prevState,
-          agent_location: `latitude:Geolocation is not supported by this browser.;longitude:Geolocation is not supported by this browser.;`
-        }
-      })
     }
-    navigator.permissions.query({name:'geolocation'}).then(function(result) {
-      // Will return ['granted', 'prompt', 'denied']
-      setForm(prevState=>{
-        return {
-          ...prevState,
-          agent_location_permission: `${result.state}`
-        }
-      })
-    });
   }, [location])
 
   /**
    * HTTP Request to `/api/index.js`
    */
   useEffect(() => {
-    if(form&&form.agent_location_permission){
-      const formBody = Object.keys(form).map(key => encodeURIComponent(key) + '=' + encodeURIComponent(form[key])).join('&')
-      const postRequestToOctopusWaterfallManagement = new Request('https://octopus-waterfall-management.herokuapp.com/api/agents', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
-        },
-        body: formBody
-      })
-  
-      if(location.pathname.toLowerCase().includes('app')){
-        /** POST to OWMB */
-        fetch(postRequestToOctopusWaterfallManagement)
-        .then(response => console.log(response))
-        .catch(err => {
-          console.log(err)
+    if(process.env.REACT_APP_USE_SNIFF_AGENT_CONTEXT_FUNCTIONALITY==='1'){
+      if(form&&form.agent_location_permission){
+        const formBody = Object.keys(form).map(key => encodeURIComponent(key) + '=' + encodeURIComponent(form[key])).join('&')
+        const postRequestToOctopusWaterfallManagement = new Request('https://octopus-waterfall-management.herokuapp.com/api/agents', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+          },
+          body: formBody
         })
-      }
-      else if(location.pathname.toLowerCase().includes('login')){
-        /** POST to OWMB */
-        fetch(postRequestToOctopusWaterfallManagement)
-        .then(response => console.log(response))
-        .catch(err => {
-          console.log(err)
-        })
+    
+        if(location.pathname.toLowerCase().includes('app')){
+          /** POST to OWMB */
+          fetch(postRequestToOctopusWaterfallManagement)
+          .then(response => console.log(response))
+          .catch(err => {
+            console.log(err)
+          })
+        }
+        else if(location.pathname.toLowerCase().includes('login')){
+          /** POST to OWMB */
+          fetch(postRequestToOctopusWaterfallManagement)
+          .then(response => console.log(response))
+          .catch(err => {
+            console.log(err)
+          })
+        }
       }
     }
   }, [location, form])
