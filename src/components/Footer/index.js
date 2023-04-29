d27db8a930188689ef94fc80163055aba34dafcc
Ujang Sarutobi <41730448+adamcanray@users.noreply.github.com>
2023-04-29 20:14:09 +0700

    Update index.js

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <footer className="bg-gray-100 dark:bg-gray-900 text-gray-500 dark:text-gray-400 py-1 text-center">
-      <p>&copy;2021 Octopus Waterfall Management | Built with &#10084; by: Muhammad Adam Canrayneldy</p>
+      <p>&copy;2021 Octopus Waterfall Management | Built with &#10084; by: Adam Canray</p>
     </footer>
   )
 }
